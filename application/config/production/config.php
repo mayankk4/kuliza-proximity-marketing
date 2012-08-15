108f83ddc2044c08d0608d0681fcfdb5e0ee1333
Mayank Kandpal <mayankk4@gmail.com>
2012-08-15 16:59:46 +0530

    Perms and prod setting

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://50.112.72.254/proximity-marketing/';
+$config['base_url']	= 'http://50.112.72.254/';
 
 /*
 |--------------------------------------------------------------------------
